f4b0c6c252d80bc43728779f7ea388245cbf1125
chenqiang2027 <cq.charle@gmail.com>
2026-01-15 09:36:57 +0800

    数据越界。

--- twoSum.cpp
+++ twoSum.cpp
@@ -29,7 +29,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size()-1;i++){
             auto it=std::find(nums.begin()+i+1,nums.end(),target-nums[i]);
             if(it!=nums.end()){
                 int j= it -nums.begin();
